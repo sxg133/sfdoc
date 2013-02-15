cd09a097f62bdef19513907fdc8c67e0856d4974
Sahil Grover <sxg133@gmail.com>
2013-02-15 09:24:35 -0500

    Added scope (class and parenthetical)

--- sfdocmaker.py
+++ sfdocmaker.py
@@ -8,7 +8,8 @@ def __get_param_content(param):
 	return '<tr><td>' + param.name + '</td><td>' + param.param_type + '</td><td>' + param.description + '</td></tr>'
 
 def __fill_in_method_content(content_method, minfo):
-	new_content = content_method.replace('[methodname]', minfo.name)
+	new_content = content_method.replace('[methodname]', minfo.name + ' (' + minfo.scope + ')')
+	new_content = new_content.replace('[methodclass]', minfo.scope)
 	param_content = [__get_param_content(p) for p in minfo.params]
 	new_content = new_content.replace('[params]', ''.join(param_content))
 	new_content = new_content.replace('[returntype]',minfo.return_type)
