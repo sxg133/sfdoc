1d9b04d27a08b993f4219b12167ba1e06ab88818
Dimitri Savelieff <savelieffd@gmail.com>
2013-02-19 15:49:51 -0500

    Removed <br> and changed noParams to no-params

--- sfdocmaker.py
+++ sfdocmaker.py
@@ -22,8 +22,8 @@ def __fill_in_method_content(content_method, minfo):
 		new_content = new_content.replace('[params]', ''.join(param_content))
 		new_content = new_content.replace('[paramsClass]', 'params')
 	else:
-		new_content = new_content.replace('[params]', '<br/><em>There are no parameters</em>')
-		new_content = new_content.replace('[paramsClass]', 'noParams')
+		new_content = new_content.replace('[params]', '<em>There are no parameters</em>')
+		new_content = new_content.replace('[paramsClass]', 'no-params')
 	new_content = new_content.replace('[returntype]', cgi.escape(minfo.return_type))
 	new_content = new_content.replace('[returndescription]',minfo.return_description)
 	return new_content
