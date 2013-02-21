7476ca412b5ffb3f4d773e4517883c8f8795e5f6
Sahil Grover <sxg133@gmail.com>
2013-02-21 13:45:31 -0500

    fixed method list to only show methods in scope

--- sfdocmaker.py
+++ sfdocmaker.py
@@ -70,7 +70,7 @@ def create_outfile(classlist, cinfo, target):
 	class_items = [__get_class_item(c) for c in classlist]
 	new_content = new_content.replace('[classlist]', ''.join(class_items))
 	new_content = new_content.replace('[indexfile]', SFDocSettings.indexfile)
-	mnamelist = ''.join(['<li class="' + m.scope + '"><a href="#' + m.name + '">' + m.name + '</a>' for m in cinfo.methods])
+	mnamelist = ''.join(['<li class="' + m.scope + '"><a href="#' + m.name + '">' + m.name + '</a>' for m in cinfo.methods if m.scope in SFDocSettings.scope])
 	new_content = new_content.replace('[methodnamelist]', mnamelist)
 	
 	if not SFDocSettings.test:
